4346927bab80a49aeac7a508486cec647e110c7d
andriydovbenko <andriydovbenko1@gmail.com>
2020-04-30 15:18:19 +0200

    created two abstcract subclasses of Power Plant -> storable and unstorable branches

---
+++ src/main/java/com/electricity/model/plant/StorageIncapableAbstractPlant.java
@@ -0,0 +1,34 @@
+package com.electricity.model.plant;
+
+import com.electricity.model.resource.UnstorableResource;
+
+public abstract class StorageIncapableAbstractPlant extends PowerPlant {
+    private UnstorableResource resource;
+
+    protected StorageIncapableAbstractPlant(UnstorableResource resource) {
+        this.resource = resource;
+    }
+
+    public UnstorableResource getResource() {
+        return resource;
+    }
+
+    public void setResource(UnstorableResource resource) {
+        this.resource = resource;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", resource=" + resource;
+    }
+}
