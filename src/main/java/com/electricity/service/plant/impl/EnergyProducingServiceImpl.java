c026b03b053afdfc361e4869d64470be7498b657
andriydovbenko <andriydovbenko1@gmail.com>
2020-05-01 18:23:02 +0200

    created EnergyProducingService for using resource and producing energy

---
+++ src/main/java/com/electricity/service/plant/impl/EnergyProducingServiceImpl.java
@@ -0,0 +1,75 @@
+package com.electricity.service.plant.impl;
+
+import com.electricity.enumeration.PowerPlantType;
+import com.electricity.model.plant.PowerPlant;
+import com.electricity.model.plant.StorageCapableAbstractPlant;
+import com.electricity.model.plant.StorageIncapableAbstractPlant;
+import com.electricity.service.plant.EnergyProducingService;
+
+import java.util.Collections;
+import java.util.EnumMap;
+import java.util.Map;
+
+public class EnergyProducingServiceImpl implements EnergyProducingService {
+    private final Map<PowerPlantType, EnergyProducingService> mappedMethods;
+    private double producedPower;
+
+    public EnergyProducingServiceImpl() {
+        this.mappedMethods = Collections.synchronizedMap(new EnumMap<>(PowerPlantType.class));
+        mappedMethods.put(PowerPlantType.COAL, this::useStorageCapablePlants);
+        mappedMethods.put(PowerPlantType.NUCLEAR, this::useStorageCapablePlants);
+        mappedMethods.put(PowerPlantType.HYDRO, this::useStorageCapablePlants);
+        mappedMethods.put(PowerPlantType.SOLAR, this::useStorageIncapablePlants);
+        mappedMethods.put(PowerPlantType.WIND, this::useStorageIncapablePlants);
+    }
+
+    @Override
+    public double produceEnergy(PowerPlant powerPlant) {
+        eraseProducedPower();
+
+        return mappedMethods.getOrDefault(powerPlant.getType(), this::skipPowerPlant)
+                .produceEnergy(powerPlant);
+    }
+
+    private double useStorageCapablePlants(PowerPlant powerPlant) {
+
+        StorageCapableAbstractPlant plant = (StorageCapableAbstractPlant) powerPlant;
+
+        int totalResourceAmount = plant.getStorage().getResource().getAmount();
+        int resourceConsumption = plant.getResourceConsumption();
+
+        if (totalResourceAmount >= resourceConsumption) {
+            plant.getStorage().getResource().setAmount(totalResourceAmount - resourceConsumption);
+            producedPower = plant.getMaxPower();
+        } else {
+            powerPlant.setWorking(false);
+        }
+
+        return producedPower;
+    }
+
+
+    private double useStorageIncapablePlants(PowerPlant powerPlant) {
+        StorageIncapableAbstractPlant plant = (StorageIncapableAbstractPlant) powerPlant;
+
+        int workTimeLeft = plant.getResource().getWorkTimeLeft();
+        int oneWorkCircle = plant.getResourceConsumption();
+
+        if (workTimeLeft >= oneWorkCircle) {
+            plant.getResource().setWorkTimeLeft(workTimeLeft - oneWorkCircle);
+            producedPower = plant.getMaxPower();
+        } else {
+            powerPlant.setWorking(false);
+        }
+
+        return producedPower;
+    }
+
+    private double skipPowerPlant(PowerPlant powerPlant) {
+        return producedPower;
+    }
+
+    private void eraseProducedPower() {
+        this.producedPower = 0;
+    }
+}
