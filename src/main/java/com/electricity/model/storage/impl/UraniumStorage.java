fef31c3d0059719e0ec0c838d5410d4dcd3e9576
andriydovbenko <andriydovbenko1@gmail.com>
2020-05-02 19:18:15 +0200

    changed access modifier in UraniumStorage

--- src/main/java/com/electricity/model/storage/impl/UraniumStorage.java
+++ src/main/java/com/electricity/model/storage/impl/UraniumStorage.java
@@ -3,7 +3,7 @@ package com.electricity.model.storage.impl;
 import com.electricity.model.storage.Storage;
 
 public class UraniumStorage extends Storage {
-    private static final int CAPACITY = 1000;
+    public static final int CAPACITY = 1000;
 
     public UraniumStorage() {
         super(CAPACITY);
