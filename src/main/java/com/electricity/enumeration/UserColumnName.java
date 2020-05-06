2b17d9b70ee16493090a8a074c9730059acebcf4
andriydovbenko <andriydovbenko1@gmail.com>
2020-05-06 19:25:10 +0200

    added new fields to UserColumnName enum

--- src/main/java/com/electricity/enumeration/UserColumnName.java
+++ src/main/java/com/electricity/enumeration/UserColumnName.java
@@ -2,8 +2,11 @@ package com.electricity.enumeration;
 
 public enum UserColumnName {
     ID("id"),
+    LOGIN("login"),
+    PASSWORD("password"),
     FIRST_NAME("firs_name"),
     LAST_NAME("last_name"),
+    TABLE_NAME("table_name"),
     CURRENT_FUNDS_AMOUNT("current_funds_amount");
 
     private final String name;
