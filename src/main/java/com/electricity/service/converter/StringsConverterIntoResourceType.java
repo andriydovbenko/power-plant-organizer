d515b32e9ad524c82fcd82586f5d240b8c36fa28
andriydovbenko <andriydovbenko1@gmail.com>
2020-05-19 14:24:07 +0200

    created String converter service

---
+++ src/main/java/com/electricity/service/converter/StringsConverterIntoResourceType.java
@@ -0,0 +1,31 @@
+package com.electricity.service.converter;
+
+import com.electricity.enumeration.PurchasableResourceType;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class StringsConverterIntoResourceType {
+    private static final List<PurchasableResourceType> purchasableResourceTypes = new ArrayList<>() {{
+        this.add(PurchasableResourceType.COAL);
+        this.add(PurchasableResourceType.URANIUM);
+    }};
+
+    private StringsConverterIntoResourceType() {
+    }
+
+    public static synchronized PurchasableResourceType convert(String type) {
+        PurchasableResourceType resourceType = null;
+
+        Optional<PurchasableResourceType> typeOptional = purchasableResourceTypes.stream()
+                .filter(pType -> (pType.toString()).equals(type))
+                .findFirst();
+
+        if (typeOptional.isPresent()) {
+            resourceType = typeOptional.get();
+        }
+
+        return resourceType;
+    }
+}
