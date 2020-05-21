46b8b77ea7d0a0c15c2c32f1f408513e36cb0518
andriydovbenko <andriydovbenko1@gmail.com>
2020-05-21 14:27:13 +0200

    created enum AESData with data for PasswordCryptographer

---
+++ src/main/java/com/electricity/enumeration/AESData.java
@@ -0,0 +1,18 @@
+package com.electricity.enumeration;
+
+public enum AESData {
+    SECRET("superSecretKey"),
+    PADDING_SCHEMA("AES/ECB/PKCS5Padding"),
+    ALGORITHM_SHA("SHA-1"),
+    ALGORITHM_AES("AES");
+
+    private final String information;
+
+    AESData(String information) {
+        this.information = information;
+    }
+
+    public String getInformation() {
+        return information;
+    }
+}
